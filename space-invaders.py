46e2d538e9182a9e0e27d9db443665ea6f817715
Jared Cook <jcook3701@gmail.com>
2020-07-26 14:37:30 -0700

    added conda env

--- space-invaders.py
+++ space-invaders.py
@@ -6,10 +6,12 @@ for i_episode in range(20):
     observation = env.reset()
     for t in range(10000):
         env.render()
-        print(observation)
+        print(env.action_space)
+        print(env.observation_space)
         action = env.action_space.sample()
+        print(action)
         observation, reward, done, info = env.step(action)
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode {} finished after {} timesteps".format(i_episode, t+1))
             break
 env.close()
